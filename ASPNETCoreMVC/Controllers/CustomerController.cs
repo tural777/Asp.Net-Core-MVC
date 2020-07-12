4e7e6b809d4dc57b9a14aa6d375606f45db6fc33
Tural Novruzov <turalinovruzov@gmail.com>
2020-07-12 17:46:36 +0400

    Add project files.

---
+++ ASPNETCoreMVC/Controllers/CustomerController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ASPNETCoreMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace ASPNETCoreMVC.Controllers
+{
+    public class CustomerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult SaveCustomer()
+        {
+            return View(new CustomerViewModel());
+        }
+
+        [HttpPost]
+        public IActionResult SaveCustomer(Customer customer)
+        {
+            return Ok();
+        }
+    }
+}
---
+++ ASPNETCoreMVC/Entities/Customer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPNETCoreMVC.Models
+{
+    public class Customer
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string City { get; set; }
+    }
+}
