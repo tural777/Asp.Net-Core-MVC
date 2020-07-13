f382c29a7c83271e0a96e5ba24d8a6ee2afd6c07
Tural Novruzov <turalinovruzov@gmail.com>
2020-07-14 00:39:27 +0400

    Add Session and SessionDemoController

---
+++ ASPNETCoreMVC/Controllers/SessionDemoController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPNETCoreMVC.Controllers
+{
+    public class SessionDemoController : Controller
+    {
+        public string Index1()
+        {
+            HttpContext.Session.SetString("Name", "Tural");
+            return "Saved";
+        }
+
+        public string Index2()
+        {
+           return HttpContext.Session.GetString("Name");
+        }
+    }
+}
--- ASPNETCoreMVC/Startup.cs
+++ ASPNETCoreMVC/Startup.cs
@@ -24,6 +24,7 @@ namespace ASPNETCoreMVC
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddSession();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -39,9 +40,12 @@ namespace ASPNETCoreMVC
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
+
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
+            app.UseSession();
             app.UseRouting();
 
             app.UseAuthorization();
